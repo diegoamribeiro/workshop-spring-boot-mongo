7e0edbaef4004eac6f19bff137da3e6adf9629fb
Diego Ribeiro <dmribeiro@gmail.com>
2020-11-05 21:03:00 -0300

    First commit

---
+++ src/test/java/com/dmribeiro87/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmribeiro87.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
