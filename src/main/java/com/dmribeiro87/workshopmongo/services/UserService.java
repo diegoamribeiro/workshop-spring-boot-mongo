39570609e22a855a4cfca9c553e7f69c79eb63ed
Diego Ribeiro <dmribeiro@gmail.com>
2020-11-05 23:22:37 -0300

    Usando UserDTO para retornar usuários

--- src/main/java/com/dmribeiro87/workshopmongo/services/UserService.java
+++ src/main/java/com/dmribeiro87/workshopmongo/services/UserService.java
@@ -2,10 +2,13 @@ package com.dmribeiro87.workshopmongo.services;
 
 import com.dmribeiro87.workshopmongo.domain.User;
 import com.dmribeiro87.workshopmongo.repository.UserRepository;
+import com.sun.jdi.ObjectCollectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +19,11 @@ public class UserService {
     public List<User> findAll(){
         return repository.findAll();
     }
+
+    public User findById(String id){
+       Optional<User> obj = repository.findById(id);
+       return obj.orElseThrow(() -> new ObjectCollectedException("Objeto não encotrado"));
+    }
+
+
 }
