fdf85f7327a063aa85c19d1c6cc073448879ccc6
Diego Ribeiro <dmribeiro@gmail.com>
2020-11-07 20:13:04 -0300

    Searching by @Query

--- src/main/java/com/dmribeiro87/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/dmribeiro87/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.dmribeiro87.workshopmongo.repository;
 import com.dmribeiro87.workshopmongo.domain.Post;
 import com.dmribeiro87.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,6 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
+    @Query("{'title':{$regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String text);
 }
